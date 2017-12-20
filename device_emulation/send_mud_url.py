b1a840726033e3b13adc1a7732506c7c12950560
Lalksy <ler2161@columbia.edu>
2017-12-20 09:57:11 -0500

    mini dhcp client

--- device_emulation/send_mud_url.py
+++ device_emulation/send_mud_url.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python
 from scapy.all import *
 
+offers = 0
+
+def pkt_callback(pkt):
+    pkt.show() # debug statement
+    if (offers == 0):
+        global offers
+        offers += 1
+        dhcp_request = Ether(dst="ff:ff:ff:ff:ff:ff") / \
+            IP(src="0.0.0.0",dst="255.255.255.255") / \
+            scapy.all.UDP(sport=68,dport=67) / \
+            scapy.all.BOOTP(chaddr='\x78\x4f\x43\x67\xb2\xcb') / \
+            scapy.all.DHCP(options=[("message-type","request"),("server_id", pkt['IP'].src), ("requested_addr", pkt['BOOTP'].yiaddr), "end"])
+        scapy.all.sendp(dhcp_request)       
+
 
 if __name__ == "__main__":
-    
     dhcp_discover = Ether(dst="ff:ff:ff:ff:ff:ff") / \
         IP(src="0.0.0.0",dst="255.255.255.255") / \
         scapy.all.UDP(sport=68,dport=67) / \
-        scapy.all.BOOTP(chaddr=RandString(12, "0123456789abcdef")) / \
-        scapy.all.DHCP(options=[("message-type","discover"), (161, "http://160.39.149.190:8000/mud/pi-cam2.json"), "end"])
+        scapy.all.BOOTP(chaddr='\x78\x4f\x43\x67\xb2\xcb') / \
+        scapy.all.DHCP(options=[("message-type","discover"), (161, "http://localhost:8000/mud/pi-cam2.json"), "end"])
+    while(offers == 0):
+        wrpcap("test.pcap", dhcp_discover)
+        scapy.all.sendp(dhcp_discover)
+        sniff(iface="en0", prn=pkt_callback, filter="port 68 and port 67", timeout=2)
+#    print "Found other dhcp server at: %s %s" % (ans[Ether].src, ans[IP].src)   
 
-    wrpcap("test.pcap", dhcp_discover)
-
+#        scapy.all.DHCP(options=[("message-type","discover"),"end"])
