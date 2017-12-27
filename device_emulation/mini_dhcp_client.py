794600e4f755b253778f1935b4488179f19491eb
Lalksy <ler2161@columbia.edu>
2017-12-27 10:29:04 -0500

    generalized client

---
+++ device_emulation/mini_dhcp_client.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+from scapy.all import *
+import sys
+import socket
+
+offers = 0
+def pkt_callback(pkt):
+    global offers  
+    #pkt.show() # debug statement
+    if (offers == 0):
+        offers += 1
+        dhcp_request = Ether(dst="ff:ff:ff:ff:ff:ff") / \
+            IP(src="0.0.0.0",dst="255.255.255.255") / \
+            scapy.all.UDP(sport=68,dport=67) / \
+            scapy.all.BOOTP(chaddr=mac) / \
+            scapy.all.DHCP(options=[("message-type","request"),("server_id", pkt['IP'].src), ("requested_addr", ip), "end"])
+        scapy.all.sendp(dhcp_request)       
+
+def valid_ip(address):
+    try:
+        socket.inet_aton(address)
+        return True
+    except:
+        return False
+
+if __name__ == "__main__":
+
+    if (len(sys.argv) != 3) :
+        print "Usage: python mini_dhcp_client.py <mac> <current ip>"
+        exit()
+    mac = sys.argv[1].replace(":", "", 5).decode('hex')
+
+    ip = sys.argv[2]
+    if(not valid_ip(ip)):
+        print "Invalid ip address."
+        exit()
+    
+    dhcp_discover = Ether(dst="ff:ff:ff:ff:ff:ff") / \
+        IP(src="0.0.0.0",dst="255.255.255.255") / \
+        scapy.all.UDP(sport=68,dport=67) / \
+        scapy.all.BOOTP(chaddr=mac) / \
+        scapy.all.DHCP(options=[("message-type","discover"), (161, "http://storm.cis.fordham.edu/~rieger/mud/mock.json"), "end"])
+
+    counts = 0
+    while(offers == 0 and counts < 5):
+        wrpcap("test.pcap", dhcp_discover)
+        scapy.all.sendp(dhcp_discover)
+        sniff(iface="en0", prn=pkt_callback, filter="port 68 and port 67", timeout=2)
+        counts += 1
