2de152f6ea466e0f9ac73adba875994aa9082922
Lalksy <ler2161@columbia.edu>
2018-02-12 11:04:18 -0500

    cleaning and commenting controller

--- static_profile/mud_controller.py
+++ static_profile/mud_controller.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+# Author: Lalka Riger
+# About: refactored controller code as module
+
 # Copyright (c) 2016, Cisco Systems
 # All rights reserved.
 # Redistribution and use in source and binary forms, with or without modification,
@@ -25,12 +28,45 @@
 ##-->>>>>>$ mud_controller.py python code to extract the ACL name and DACL content from MUD server$<<<<<<<--##
 ##-->>>>>>$ MUD file reference https://www.ofcourseimright.com/mudmaker/ $<<<<<<<<<--##
 
-from urllib2 import urlopen, URLError, HTTPError
+#from urllib2 import urlopen, URLError, HTTPError
 from subprocess import call
 import sys
 import os
 import json
 import glob
+
+def mac_auth(mac, sig):
+    # verify the sha256 signature on mac
+    # mac and sig are text-encoded for transmission
+    dir_input = "./"
+    key = dir_input+"pub.key"
+
+    f = open("mac.txt.tmp", 'w')
+    n = f.write(mac)
+    f.close()
+
+    f = open("sign.txt.tmp", 'w')
+    f.write(sig)
+    f.close()
+
+    # convert signature to openssl-pasrasble format
+    converted = call(['base64', '-D', './sign.txt.tmp', '-o', 'sign.txt.sha256.tmp'])
+
+    ver = call(['openssl', 'dgst', '-sha256','-verify', key, '-signature', 'sign.txt.sha256.tmp', 'mac.txt.tmp'])
+    
+    #remove temporary files
+    clean_tmp()
+
+    return ver
+
+def clean_tmp():
+    # remove temporary files
+    try:
+        os.remove("mac.txt.tmp")
+        os.remove("sign.txt.tmp")
+        os.remove("sign.txt.sha256.tmp")
+    except OSError:
+        pass
 
 def dlfile(device_json, device_ps7):
     # Open the url
@@ -71,11 +107,11 @@ def dlfile(device_json, device_ps7):
         return decrypted
         #delete(device_name) # remove old download files
     #handle errors
-    except HTTPError, e:
-        print "HTTP Error:", e.code, device_json, device_ps7
+    except HTTPError as e:
+        print("HTTP Error:", e.code, device_json, device_ps7)
         return 1
-    except URLError, e:
-        print "URL Error:", e.reason, device_json, device_ps7
+    except URLError as e:
+        print("URL Error:", e.reason, device_json, device_ps7)
         return 1
 
 def delete(device_name_del):
@@ -90,7 +126,7 @@ def delete(device_name_del):
                 if os.path.exists(filename):
                     file_list.append(filename)
                 else:
-                    print 'No files path found +name'
+                    print('No files path found +name')
     for device_file in file_list:
         os.remove(device_file)
                         
@@ -102,7 +138,7 @@ def radius(device_url):
         device_name_ps7 = device_url_ps7.split("mud/",1)[1]
         device_name_ps7_format = os.path.splitext(device_name_ps7)[0]
         device_ps7 = device_url_ps7+".p7s"
-        print device_ps7
+        print(device_ps7)
     else: 
         device_ps7 = device_url+".p7s/"
     return dlfile(device_json, device_ps7)
@@ -113,7 +149,7 @@ def get_json_value(json_object, index):
         with open(mud_file_store, 'r') as f:
             data = f.read()
     except IOError:
-        print 'cannot open file to read', mud_file_store
+        print('cannot open file to read', mud_file_store)
     else:
         data = json.loads(data)
     in_acl = (data['ietf-access-control-list:access-lists']['acl'][0]['aces']['ace'])
@@ -186,7 +222,7 @@ def get_json_value(json_object, index):
         try:
             return new_list[index]
         except IndexError:
-            print ""
+            print ("")
 
 def read_json():
     rules = []
